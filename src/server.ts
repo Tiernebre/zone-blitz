fbaba7b85beea4ec4df469957794e159c2f682e7
Brendan Tierney <tiernebre@gmail.com>
2024-07-11 18:14:39 -0600

    remove debug log

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,6 @@ import { register, registrationPage } from "./registration/registration.ts";
 export const start = () =>
   Deno.serve((request) => {
     if (request.method === "POST") {
-      console.log("form?");
       return register(request);
     } else {
       return registrationPage();
