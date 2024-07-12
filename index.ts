be9208e6709762711f817b4701ab7d33c3605515
Brendan Tierney <tiernebre@gmail.com>
2024-07-12 17:09:51 +0000

    connects to postgres

--- index.ts
+++ index.ts
@@ -1,3 +1,15 @@
 import { start } from "./src/server.ts";
+import postgres from "postgres";
 
 start();
+
+const sql = postgres({
+  host: "db",
+  port: 5432,
+  user: "postgres",
+  password: "example",
+  database: "postgres",
+});
+
+const result = await sql`SELECT 1`;
+console.log({ result });
