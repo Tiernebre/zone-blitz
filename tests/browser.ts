a549b85a69f2b1651db68a7a677e4cdea993a16a
Brendan Tierney <tiernebre@gmail.com>
2024-07-15 01:29:17 +0000

    browser test

--- tests/browser.ts
+++ tests/browser.ts
@@ -1,7 +1,15 @@
-import { launch } from "@astral/astral";
+import { launch, Page } from "@astral/astral";
 
-export const browser = () =>
-  launch({
+export const browserTest = async (
+  url: string,
+  cb: (page: Page) => void | Promise<void>,
+) => {
+  const browser = await launch({
     path: "/usr/local/bin/chrome",
     args: ["--no-sandbox"],
   });
+  const page = await browser.newPage(url);
+  await cb(page);
+  await page.close();
+  await browser.close();
+};
--- tests/registration.ui.test.ts
+++ tests/registration.ui.test.ts
@@ -1,6 +1,6 @@
 import { start } from "../src/server.ts";
 import { assert, assertNotEquals } from "@std/assert";
-import { browser } from "./browser.ts";
+import { browserTest } from "./browser.ts";
 import { REGISTRATION_URL as URL } from "./utils.ts";
 import { Page } from "@astral/astral";
 
@@ -14,31 +14,25 @@ const getPasswordInput = (page: Page) => page.$('input[name="password"]');
 const getButton = (page: Page) => page.$("button");
 
 Deno.test("renders a form", async () => {
-  const suite = await browser();
-  const page = await suite.newPage(URL);
-  assertNotEquals(await getUsernameInput(page), null);
-  await page.close();
-  await suite.close();
+  await browserTest(URL, async (page) => {
+    assertNotEquals(await getUsernameInput(page), null);
+  });
 });
 
 Deno.test("registers a user", async () => {
-  const suite = await browser();
-  const page = await suite.newPage(URL);
-  await (await getUsernameInput(page))!.type(crypto.randomUUID());
-  await (await getPasswordInput(page))!.type(crypto.randomUUID());
-  await (await getButton(page))!.click();
-  await page.waitForNavigation();
-  assert((await page.content()).includes("registered"));
-  await page.close();
-  await suite.close();
+  await browserTest(URL, async (page) => {
+    await (await getUsernameInput(page))!.type(crypto.randomUUID());
+    await (await getPasswordInput(page))!.type(crypto.randomUUID());
+    await (await getButton(page))!.click();
+    await page.waitForNavigation();
+    assert((await page.content()).includes("registered"));
+  });
 });
 
 Deno.test("validates that username must be filled out", async () => {
-  const suite = await browser();
-  const page = await suite.newPage(URL);
-  await (await getPasswordInput(page))!.type(crypto.randomUUID());
-  await (await getButton(page))!.click();
-  assertNotEquals(await getInvalidUsernameInput(page), null);
-  await page.close();
-  await suite.close();
+  await browserTest(URL, async (page) => {
+    await (await getPasswordInput(page))!.type(crypto.randomUUID());
+    await (await getButton(page))!.click();
+    assertNotEquals(await getInvalidUsernameInput(page), null);
+  });
 });
